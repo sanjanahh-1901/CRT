954e324b8c51dc62b489f4e5f50443fc71581faa
Sanjana <bobbidysanjana@gmail.com>
2026-03-02 10:03:14 +0530

    done

--- IV-SEM/Python/Assignment/M01_Language_&_Programming_Basics/AST03/task.py
+++ IV-SEM/Python/Assignment/M01_Language_&_Programming_Basics/AST03/task.py
@@ -1,8 +1,21 @@
-name = input()
-n1,n2,n3 = list(map(int,input().split()))
-average = (n1+n2+n3)/3
-if average >= 50:
-    status = "Pass"
-else:
-    status = "Fail"
-print("Average grade:", average, "Status:", status)
+def Student_Grade_System(name:str,n1: int,n2: int,n3: int) -> str:
+   average = (n1+n2+n3)/3
+   if average >= 50:
+      status = "Pass"
+   else:
+      status = "Fail"
+   return f"Average grade: {average}, Status: {status}"
+
+
+if __name__ == '__main__':
+    name = input()
+    n1,n2,n3 = list(map(int,input().split()))
+    print(Student_Grade_System(name,n1,n2,n3))
+
+
+
+
+
+
+
+
--- IV-SEM/Python/Assignment/M01_Language_&_Programming_Basics/AST04/task.py
+++ IV-SEM/Python/Assignment/M01_Language_&_Programming_Basics/AST04/task.py
@@ -1,3 +1,14 @@
-str = input()
-for i in range(len(str)):
-      print(str[len(str)-1-i], end="")
+def Reverse_String(s: str) -> str:
+      reversed_str = ""
+      for i in range(len(s)):
+            reversed_str += s[len(s)-1-i]
+      return reversed_str
+
+if __name__ == '__main__':
+    s = input()
+    print(Reverse_String(s))
+
+
+
+
+
