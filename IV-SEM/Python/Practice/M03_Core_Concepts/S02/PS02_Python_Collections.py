ba6044eca6452b8623d17b55551fa710522df030
Sanjana Bobbidy <2401100cse0107@cse.suh.edu.in>
2026-03-09 10:41:18 +0530

    done

---
+++ IV-SEM/Python/Practice/M03_Core_Concepts/S02/PS02_Python_Collections.py
@@ -0,0 +1,103 @@
+'''
+Python Collections
+
+
+#1)
+
+
+#2) Accessing a list:
+a = [10, 20, 30, 40, 50]
+print(a[0])  # O(1)
+print(a[-1])  # O(1)
+
+#3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+#4) Adding elements to a list:
+a = [10, 20, 30, 40, 50]
+a.append(100)  # O(1)
+
+
+
+
+#5) Removing elements from a list:
+a = [10, 20, 30, 40, 50]
+a.remove(40)  # O(n)
+print(a)
+a.pop()  # O(1)
+print(a)
+
+#6) Slicing:
+a = [10, 20, 30, 40, 50]
+print(a[0:])  # O(k) where k is the size of the slice
+print(a[2:])  # O(k) where k is the size of the slice
+#reversing a list using slicing
+print(a[::-1]) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#5) Set Operations:
+a = set([10, 20, 30, 450])
+b = set([20, 30, 40, 50])
+
+
+
+#7) LeetCode 169
+
+#8) LeetCode 88
+
+#9) LeetCode 268
+
+#10) LeetCode 575
