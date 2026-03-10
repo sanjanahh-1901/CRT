ddb9dd0cfe4c4abcf6cf286a456db14208a0d6b2
Sanjana Bobbidy <2401100cse0107@cse.suh.edu.in>
2026-03-11 01:05:36 +0530

    done

--- IV-SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST05/task.py
+++ IV-SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST05/task.py
@@ -15,13 +15,3 @@ if __name__ == '__main__':
     n = int(input())
     print(Collatz_Sequence(n))
 
-
-n = int(input())
-while n != 1:
-        if n % 2 == 0:
-              n = n // 2
-        else:
-             n = 3 * n + 1 
-        print(n, end=" ") 
-
-        
